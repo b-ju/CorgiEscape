a1f223c1a9d36657ab129816171494c5b628f76a
Ben <benjurenka@gmail.com>
2020-04-10 14:05:00 -0600

    Initial Commit

---
+++ Graphics Example/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graphics Example
+//
+//  Created by ben jurenka on 4/10/20.
+//  Copyright © 2020 ben jurenka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
